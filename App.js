5a2c6252e16616772234a03c3e3ede77dca7cca9
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2019-03-06 21:09:59 +0530

    repos fetched

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, Text, TextInput, View,StyleSheet } from 'react-native';
+import { FlatList, TouchableOpacity, Text, TextInput, View,StyleSheet } from 'react-native';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {text: ''};
+    this.state ={ isLoading: true};
+  }
+
+  onPress = () => {
+    fetch('https://api.github.com/users/ashutoshkumarsingh/repos')
+    .then((response) => response.json())
+    .then((responseJson) => {
+
+      this.setState({
+        isLoading: false,
+        dataSource: responseJson,
+      }, function(){
+      });
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
   }
 
   render() {
@@ -16,9 +33,16 @@ export default class App extends Component {
         />
         <TouchableOpacity
          style={styles.button}
-       >
+         onPress={this.onPress}
+        >
          <Text>Search</Text>
        </TouchableOpacity>
+
+       <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.name}, {item.id}</Text>}
+          keyExtractor={({id}, index) => id}
+        />
        
       </View>
     );
